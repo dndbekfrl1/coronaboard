35af0935c1b45c171536b17213ce347bfe6d0d24
dndbekfrl1 <dndbekfrl1@gmail.com>
2020-06-21 19:19:48 +0900

    /newsdata

--- extest/public/js/covid-board.js
+++ extest/public/js/covid-board.js
@@ -1,35 +1,26 @@
 /* 확진자 정보 */
 $(function(){
+    console.log("??");
     var settings = {
         "url": "https://api.covid19api.com/summary",
         "method": "GET",
         "timeout": 0,
       };
-
       $.ajax(settings).done(function (response) {
         var Global = 'Global'; Global=response[Global];
         var Countries='Countries';Countries=response[Countries];
-        /* 한국 */
-        var korea_south=Countries[88];
-        //누적 확진자 
-        $(".live-confirmed").
-        text("+"+korea_south.NewConfirmed+"명");
-        //새 확진자 
-        $(".changed-confirmed").text(korea_south.TotalConfirmed+"명");
-        //새 확진자 증가 원그래프 애니메이션 
-        var inc=korea_south.NewConfirmed*1;
-        $(".korea-changed").animate({
-            width: inc+'%',
-            height: inc+'%'
-        });
-        //////////////
+       
         /* 전세계 확진자 순 나열 */ 
         $(".NewConfirmed").text(Global.NewConfirmed+" 명");
         Countries.sort(function(a,b){return b.TotalConfirmed-a.TotalConfirmed;});
         console.log(Countries);
    
         //board for문으로 수정해야함...
-        for(var i=0;i<5;i++){
+
+        var cell=document.getElementById('worldwide-board-cell');
+        for(var i=0;i<Countries.length();i++){
+            cell = cell.cloneNode(true);
+            document.getElementById("worldwide-board").appendChild(cell);
             $(".worldwide-board li:nth-child("+(i+1)+")").children('.rank').text(i+1);
             $(".worldwide-board li:nth-child("+(i+1)+")").children('.country').text(Countries[i].Country);
             $(".worldwide-board li:nth-child("+(i+1)+")").children('.confirmed').children('.total-confirmed').text(Countries[i].TotalConfirmed+"명");
@@ -43,23 +34,4 @@ $(function(){
             console.log(i);
         }
       }); 
-});
-
-/* 뉴스 */
-$(function(){
-    var settings = {
-        "url": "/newsdata",
-        "method": "GET",
-        "timeout": 0,
-      };
-    $.ajax(settings).done(function (response) {
-        NewsData=response;
-        /*관련 뉴스 */
-         for(var  i=0;i<5;i++){
-            $( 'ul > li:nth-child('+i+') > a').attr('href',NewsData[i].Link);
-            $( 'ul > li:nth-child('+i+') > a > div.headline').text(NewsData[i].Headline);
-            $( 'ul > li:nth-child('+i+') > a > div.summary').text(NewsData[i].Summary);
-            $( 'ul > li:nth-child('+i+') > a > div.date').text(NewsData[i].Date);
-        }
-    });
 });
--- extest/router/main.js
+++ extest/router/main.js
@@ -2,40 +2,13 @@ module.exports = function (app) {
    app.get('/', function (req, res) {
       res.render('index.html')
    });
-   /*다이나믹 서비스*/
-   app.get('/dynamic', function(req, res){
-      var lis = '';
-      for(var i = 0; i <5; i++){
-          lis += '<li>coding ' + i + '</li>';
-      }
-      // 자바스크립트 새로운 표준 formatted text 기능
-      // ` `(grave accent) 사용을 통해서 JS에서 여려줄의 코드를 넣을 수 없는 문제를 해결
-      var output =
-      `<!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          hello Dynamic html~~!
-              <ul>
-                  ${lis} <!--문자열 내에서 변수 사용-->
-              </ul>
-        </body>
-      </html>`;
-      res.send(output);
-    });
-    
+     
    /* 뉴스 데이터 크롤링 */
-   app.get('/test', function (req, res) {
-      console.log("testing ...");
+   app.get('/newsdata', function (req, res) {
       var request = require('request');
       var cheerio = require('cheerio');
-
-      var fs = require('fs');
-
+      //var fs = require('fs');
       request.get({ url: 'https://www.ytn.co.kr/issue/corona.php' }, function (err, response, body) {
-
          var $ = cheerio.load(body);
          var NewsArray = new Array();
          var headline; var summary; var date; var link;
@@ -52,25 +25,27 @@ module.exports = function (app) {
             date = $(' #ytn_list_v2014 > dl:nth-child(' + i + ') >dd.date').text();
             //기사 링크
             link += $('#ytn_list_v2014 > dl:nth-child(' + i + ') > dd.text > a').attr('href');
-            News.Headline = headline;
-            News.Summary = summary;
-            News.Date = date;
-            News.Link = link;
-
-            console.log(i);
-            NewsArray.push(News);
+            News.Headline = headline; News.Summary = summary; News.Date = date; News.Link = link;
+            
+            console.log(i);NewsArray.push(News);
+            //res.send(News);
          }
 
          console.log(NewsArray);
-         fs.writeFile("newsdata.js", "var NewsData =" + JSON.stringify(NewsArray), function (err) {
-            if (err) throw err;
-            console.log('complete');
-         }
-         );
+         // fs.writeFile("newsdata.js", "var NewsData =" + JSON.stringify(NewsArray), function (err) {
+         //    if (err) throw err;
+         //    console.log('complete');
+         // }
+         // );
+         res.send(NewsArray);
       })
+   });
 
-      res.send(NewsArray);
-
+   app.get('/covid-board.html',function(req,res){
+      res.render('covid-board.html');
+   });
+   app.get('/covid-news.html',function(req,res){
+      res.render('covid-news.html');
    });
 
 }
